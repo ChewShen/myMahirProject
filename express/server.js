e304673ed688c1998c8cb1f8c26178b9a7c43dbf
ChewShen <107638362+ChewShen@users.noreply.github.com>
2026-05-29 18:36:26 +0800

    added some page

--- express/server.js
+++ express/server.js
@@ -3,6 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.engine( 'ejs', require( 'ejs' ).__express);
+app.set( 'view engine', 'ejs' );
+
+app.set( 'views', path.join( __dirname, 'views' ));
+
 
 app.use( express.static( 'public' ));
 
