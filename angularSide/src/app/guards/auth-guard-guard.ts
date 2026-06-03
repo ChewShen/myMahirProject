a221da82d2de2b4e16240d09128dcb3d959f2d9a
ChewShen <chewshen012@gmail.com>
2026-06-03 23:12:28 +0800

    delete unrelated file

--- angularSide/src/app/guards/auth-guard-guard.ts
+++
@@ -1,6 +0,0 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-
-export const authGuardGuard: CanActivateFn = (route, state) => {
-  const router = inject(Router);
-};
