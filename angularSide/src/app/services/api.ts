86583b86397aa5388c08a3287edc3e03c357e729
ChewShen <chewshen012@gmail.com>
2026-06-07 19:47:56 +0800

    added more flexible between working in dev and prod with environments

--- angularSide/src/app/services/api.ts
+++ angularSide/src/app/services/api.ts
@@ -1,28 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class Api {
-  private apiUrl = 'http://localhost:3000/api/courses';
+  private apiUrl = environment.apiUrl
 
   constructor(private http: HttpClient) { }
 
   getCourses(): Observable<any> {
-    return this.http.get(this.apiUrl);
+    return this.http.get<any>(`${this.apiUrl}/courses`);
   }
 
   generateQuiz(courseContent: string) {
-    return this.http.post<any>('/api/courses/generate-quiz', {
+    return this.http.post<any>(`${this.apiUrl}/courses/generate-quiz`, {
       courseContent: courseContent
     });
   }
 
   saveScore(courseId: number, score: number, totalQuestions: number) {
-    return this.http.post<any>('api/courses/save-score', { 
+    return this.http.post<any>(`${this.apiUrl}/courses/save-score`, { 
       courseId: courseId,
       score: score,
       totalQuestions: totalQuestions
@@ -30,35 +31,36 @@ export class Api {
   }
 
   login(userData: any) {
-    return this.http.post<any>('/api/login', userData);
+    // return this.http.post<any>('/api/login', userData);
+    return this.http.post<any>(`${this.apiUrl}/login`, userData);
   }
 
   register(userData: any) {
-    return this.http.post<any>('/api/register', userData);
+    return this.http.post<any>(`${this.apiUrl}/register`, userData);
   }
 
   getAllScores(): Observable<any> {
-    return this.http.get<any>('/api/admin/scores');
+    return this.http.get<any>(`${this.apiUrl}/admin/scores`);
   }
 
   createCourse(courseData: any): Observable<any> {
-    return this.http.post<any>('/api/admin/courses', courseData);
+    return this.http.post<any>(`${this.apiUrl}admin/courses`, courseData);
   }
 
   getCourseById(id: string | number): Observable<any> {
-    return this.http.get<any>(`/api/courses/${id}`);
+    return this.http.get<any>(`${this.apiUrl}/courses/${id}`);
   }
 
   getMyScores(): Observable<any> {
-    return this.http.get<any>('/api/courses/my-scores');
+    return this.http.get<any>(`${this.apiUrl}/courses/my-scores`);
   }
 
   deleteCourse(courseId: string | number): Observable<any> {
-    return this.http.delete<any>(`/api/admin/courses/${courseId}`);
+    return this.http.delete<any>(`${this.apiUrl}/admin/courses/${courseId}`);
   }
 
   updateCourse(courseId: string | number, courseData: any): Observable<any> {
-    return this.http.put<any>(`/api/admin/courses/${courseId}`, courseData);
+    return this.http.put<any>(`${this.apiUrl}/admin/courses/${courseId}`, courseData);
   }
   
 }
---
+++ angularSide/src/environments/environments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api' // Points straight to native Express port 3000
+};
